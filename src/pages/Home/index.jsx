6994fd5bcf354315a3e80980322cb1e9dec25e34
lucaslira <lucasliraone@gmail.com>
2023-09-21 12:49:07 -0300

    fixing message error '{} missing in props validation' - Header component name to Button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import { Container } from './styles'
-import { Button } from '../../components/Button'
+import { Button } from '../../components/Header'
 import { FiPlus } from 'react-icons/fi'
 
 export function Home(){
     return(
         <Container>
-            <Button title="Adicionar filme" icon={FiPlus}/>        </Container>
+            <Button title="Adicionar filme" icon={FiPlus}/>
+        </Container>
     )
 }
